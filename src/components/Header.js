0cdc4f9b52417931bf3c6c0cade2305c5ff34aa5
Matteo Rubino <82002278+zVandre@users.noreply.github.com>
2025-02-21 21:14:28 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,14 @@ import './Header.css';
 const Header = () => {
   return (
     <header className="header">
-      <div className="logo">TechTalk Festival</div>
+      <div className="logo">TechTalk Festival 2025</div>
       <nav>
         <ul>
           <li><a href="#hero">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#schedule">Schedule</a></li>
-          <li><a href="#contact">Contact</a></li>
+          <li><a href="#program">Programma</a></li>
+          <li><a href="#speakers">Speaker</a></li>
+          <li><a href="#workshops">Workshop</a></li>
+          <li><a href="#contact">Contatti</a></li>
         </ul>
       </nav>
     </header>
